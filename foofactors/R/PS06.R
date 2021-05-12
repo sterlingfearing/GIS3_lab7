5ba57f4cd4b2eb685ae3af30e62e9650ca7ea33f
sterlingfearing <77306903+sterlingfearing@users.noreply.github.com>
2021-05-12 15:03:12 -0700

    Upload files created following lab video

---
+++ foofactors/R/PS06.R
@@ -0,0 +1,15 @@
+#' Medicaid Expenditure 2019
+#'
+#' A data set containing medicaid expenditure data at the state level for 2019.
+#'
+#' @format a data frame with 51 rows and 4 variables:
+#' \describe{
+#'     \item {STATEFP}{STATEFP}
+#'     \item {TtlMedExpC}{Total Medicaid spending in character format}
+#'     \item {TtlMedExpN}{Total Medicaid spending in numeric format}
+#'     \item {Year}{Year of the data}
+#'     }
+#'
+#'@source \url{https://www.kff.org/medicaid/state-indicator/total-medicaid-spending/?currentTimeframe=0&sortModel=%7B%22colId%22:%22Location%22,%22sort%22:%22asc%22%7D}
+#'
+"PS06"
